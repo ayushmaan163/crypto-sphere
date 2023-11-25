90b45a3fdce91a1e89fc88f067d4f8a12a1573c3
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-11-25 16:34:47 +0530

    Generating Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,28 +3,54 @@ import {
   Container,
   MenuItem,
   Select,
+  ThemeProvider,
   Toolbar,
   Typography,
+  createTheme,
 } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  // const appBarStyle = {
-  //   backgroundColor: "#006DF0",
-  // };
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
 
   return (
-    <AppBar style={{ backgroundColor: "#006DF0" }} position="static">
-      <Container>
-        <Toolbar>
-          <Typography>Crypto Sphere</Typography>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar style={{ backgroundColor: "#006DF0" }} position="static">
+        <Container>
+          <Toolbar>
+            <Link to="/" style={{ flex: 1 }}>
+              <Typography
+                style={{
+                  fontFamily: "Montserrat",
+                  fontWeight: "bold",
+                  fontSize: "x-large",
+                  cursor: "pointer",
+                  color: "white",
+                }}
+              >
+                Crypto Sphere
+              </Typography>
+            </Link>
 
-          <Select variant="outlined">
-            <MenuItem>INR</MenuItem>
-            <MenuItem>USD</MenuItem>
-          </Select>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginLeft: 15,
+              }}
+            >
+              <MenuItem value={"INR"}>INR</MenuItem>
+              <MenuItem value={"USD"}>USD</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 export default Header;

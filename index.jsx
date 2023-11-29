0cc2aa96d29064fba7686cca3b0b1681a39fa535
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-11-30 01:56:36 +0530

    Import Alice Carousel

--- index.jsx
+++ index.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import appRouter from "./src/App";
 import { RouterProvider } from "react-router-dom";
+import "react-alice-carousel/lib/alice-carousel.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={appRouter} />);

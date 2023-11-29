4a707d54e1d939f2c88b1833a4a61daec1e04d3d
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-11-29 18:24:20 +0530

    Add all styling & Typography

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,54 @@
+import { Typography } from "@mui/material";
+import Banner from "../components/Banner";
+import styled from "@emotion/styled";
+
+const BannerContent = styled("div")({
+  height: 400,
+  display: "flex",
+  flexDirection: "column",
+  paddingTop: 25,
+  justifyContent: "space-around",
+});
+
+const Tagline = styled("div")({
+  display: "flex",
+  height: "40%",
+  flexDirection: "column",
+  justifyContent: "center",
+  textAlign: "center",
+});
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <Banner>
+        <BannerContent>
+          <Tagline>
+            <Typography
+              variant="h2"
+              style={{
+                fontWeight: "bold",
+                marginBottom: 15,
+                fontFamily: "Montserrat",
+              }}
+            >
+              Crypto Sphere
+            </Typography>
+
+            <Typography
+              variant="subtitle2"
+              style={{
+                color: "darkgrey",
+                textTransform: "capitalize",
+                fontFamily: "Montserrat",
+              }}
+            >
+              Get all the info regarding your favorite Crypto Currency
+            </Typography>
+          </Tagline>
+        </BannerContent>
+      </Banner>
+    </>
+  );
 };
 export default HomePage;

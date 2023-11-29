5f052c825b27b2beaf787b536ded3ac6ccafad9e
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-11-29 18:17:52 +0530

    Add banner component for the background

---
+++ src/components/Banner.jsx
@@ -0,0 +1,8 @@
+import banner from "../assets/banner2.jpg";
+import styled from "@emotion/styled";
+
+const Banner = styled("div")({
+  backgroundImage: `url(${banner})`,
+});
+
+export default Banner;

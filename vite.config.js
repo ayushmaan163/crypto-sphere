20563ed5e7374c515ce822040e8c37f61a9eca05
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-11-30 17:52:25 +0530

    Removed parser config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ["chart.js", "react-chartjs-2", "react-html-parser"],
+    exclude: ["chart.js", "react-chartjs-2"],
   },
 });

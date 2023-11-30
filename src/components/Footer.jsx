cb45f3c55a7c13244f1a046e2a2ee74536881b41
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-11-30 11:46:13 +0530

    Complete Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,52 @@
+import styled from "@emotion/styled";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const FooterStyle = styled("div")({
+  backgroundColor: "inherit",
+  boxShadow: "10%",
+  position: "static",
+  bottom: 0,
+  height: "3.75rem",
+  marginTop: "1rem",
+  width: "100%",
+  fontFamily: "MontSerrat",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <FooterStyle>
+        <div className="footer">
+          Created By{" "}
+          <i class="fa-solid fa-heart fa-flip" style={{ color: "red" }}></i>{" "}
+          <a
+            href="https://www.flaticon.com/free-icons/heart"
+            title="heart icons"
+          ></a>
+          <a
+            href="https://github.com/ayushmaan163"
+            target="_blank"
+            title="Ayushmaan Jaiswal's GitHub Profile"
+          >
+            Ayushmaan Jaiswal
+          </a>{" "}
+          <i className="fa-solid fa-copyright"></i> 2023{" "}
+          <strong>
+            Crypto<span>Sphere</span>
+          </strong>
+        </div>
+      </FooterStyle>
+    </ThemeProvider>
+  );
 };
+
 export default Footer;
